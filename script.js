31b04c81f51e6552cf2171f879b063768a4fb1b9
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-06-02 22:22:51 -0400

    continue to plan out project

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var apiURl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+var apiKey = 'a8d4d4e0925590d16cc122c92ab2023c';
+var coordsUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}`;
 var temp = data.current.temp;
 var wind = data.current.wind_speed;
 var uvi = data.current.uvi;
@@ -21,7 +23,16 @@ function init() {
             });
     }
 }
+
+
 init();
+
+temp.textContent = "Temp: " + temp;
+tempEl.textContent = "Temp: " + temp;
+windEl.textContent = "Wind: " + wind;
+uviEl.textContent = "UV Index: " + uvi;
+humidityEl.textContent = "Humidity: " + temp;
+document.body.append(tempEl, windEl, uviEl, humidityEl)
 // fetch('')
 // .then(function (response) {
 //     return response.json();
