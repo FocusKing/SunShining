3c79856e9ea64859ea29acbfef941294b371196b
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-06-05 19:29:55 -0400

    reworking project according to class videos

--- script.js
+++ script.js
@@ -1,42 +1,71 @@
-var apiURl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
-var apiKey = 'a8d4d4e0925590d16cc122c92ab2023c';
-var coordsUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}`;
-var temp = data.current.temp;
-var wind = data.current.wind_speed;
-var uvi = data.current.uvi;
-var humidity = data.current.humidity;
-var tempEl = document.createElement('p');
-var windEl = document.createElement('p');
-var uviEl = document.createElement('p');
-var humidityEl = document.createElement('p');
+// var apiURl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
+// var apiKey = 'a8d4d4e0925590d16cc122c92ab2023c';
+// var coordsUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}`;
+// var temp = data.current.temp;
+// var wind = data.current.wind_speed;
+// var uvi = data.current.uvi;
+// var humidity = data.current.humidity;
+// var tempEl = document.createElement('p');
+// var windEl = document.createElement('p');
+// var uviEl = document.createElement('p');
+// var humidityEl = document.createElement('p');
 
+// function init() {
+
+//     function getWeatherData(value) {
+//         fetch(`http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}`)
+//             .then(function (response) {
+//                 return response.json();
+//             })
+//             .then(function (data) {
+//                 var lat = data [0].lat;
+//                 var lon = data [0].lon;
+//                 showWeather(lat, lon);
+//                 storeCity(value);
+
+
+//             });
+//     }
+// }
+
+
+// init();
+
+// temp.textContent = "Temp: " + temp;
+// tempEl.textContent = "Temp: " + temp;
+// windEl.textContent = "Wind: " + wind;
+// uviEl.textContent = "UV Index: " + uvi;
+// humidityEl.textContent = "Humidity: " + temp;
+// document.body.append(tempEl, windEl, uviEl, humidityEl)
+// // fetch('')
+// // .then(function (response) {
+// //     return response.json();
+// // })
+// // .then(function (data) {
+
+// // })
 function init() {
 
-    function getWeatherData(lat, lon) {
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid={API key}`)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
 
-
-            });
-    }
+fetch('https://api.github.com/orgs/twitter/public_members')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('Twitter Public Members: Raw data \n----------');
+    console.log(data);
+  });
 }
 
+function getWeatherData() {
+fetch('https://api.github.com/orgs/twitter/repos')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('Twitter Repositories: Names only \n----------');
+   
+  });
+}
 
-init();
-
-temp.textContent = "Temp: " + temp;
-tempEl.textContent = "Temp: " + temp;
-windEl.textContent = "Wind: " + wind;
-uviEl.textContent = "UV Index: " + uvi;
-humidityEl.textContent = "Humidity: " + temp;
-document.body.append(tempEl, windEl, uviEl, humidityEl)
-// fetch('')
-// .then(function (response) {
-//     return response.json();
-// })
-// .then(function (data) {
-
-// })
+  init();
